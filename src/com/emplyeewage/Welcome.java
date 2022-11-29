0ab6df12325e0e9e80ae9dfa7a62dec28d3bfa7f
jayashri23 <jsjankar23@gmail.com>
2022-11-29 11:32:59 +0530

    Welcome To Employee Wage Computation Problems

---
+++ src/com/emplyeewage/Welcome.java
@@ -0,0 +1,8 @@
+package com.emplyeewage;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Problems");
+    }
+}
