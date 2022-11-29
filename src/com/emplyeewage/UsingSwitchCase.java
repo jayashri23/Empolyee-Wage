f751c51875d5d7db19d3329a7de242b9830b2020
jayashri23 <jsjankar23@gmail.com>
2022-11-29 12:41:34 +0530

    Calculating Employee Wage using if else

---
+++ src/com/emplyeewage/UsingSwitchCase.java
@@ -0,0 +1,32 @@
+package com.emplyeewage;
+
+import java.util.Random;
+
+public class UsingSwitchCase {
+
+    public static void main(String[] args) {
+        int wagePerHour =20;
+        Random rand = new Random();
+        int check = rand.nextInt(3);
+        int workingHours;
+        if (check == 1) {
+            workingHours = 16;
+            System.out.println("Employee is Present Full Time");
+            int empWage = wagePerHour * workingHours;
+            System.out.println(" Employee Wage = " + empWage);
+        }
+        else if (check == 2){
+            workingHours = 8;
+            System.out.println("Employee is Present Part Time ");
+            int empWage = wagePerHour * workingHours;
+            System.out.println(" Employee Wage = " + empWage);
+        }
+        else {
+            workingHours = 0;
+            System.out.println("Employee is Absent ");
+            int dailyEmpWage = wagePerHour*workingHours;
+            System.out.println(" Employee Wage = " + dailyEmpWage);
+        }
+        System.out.println("-------------------------------------");
+    }
+}
